9b3c262e0d68494fed6d0b1d1393b9dcec34ae8d
Dimitar Andreev <dimitarandr@yahoo.com>
2025-11-23 17:54:38 +0100

    feat(config): add DocuSign JWT configuration with Zod validation

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -12,9 +12,13 @@ export default () => {
       integrationKey: env.DOCUSIGN_INTEGRATION_KEY,
       userId: env.DOCUSIGN_USER_ID,
       authServer: env.DOCUSIGN_AUTH_SERVER,
+      apiBase: env.DOCUSIGN_API_BASE,
       scopes: env.DOCUSIGN_SCOPES.split(' '),
       privateKeyPath: env.DOCUSIGN_PRIVATE_KEY_PATH,
       tokenExpiration: parseInt(env.DOCUSIGN_TOKEN_EXPIRATION, 10),
+      clientSecret: env.DOCUSIGN_CLIENT_SECRET,
+      redirectUri: env.DOCUSIGN_REDIRECT_URI,
+      accountId: env.DOCUSIGN_ACCOUNT_ID,
     },
   };
 };
